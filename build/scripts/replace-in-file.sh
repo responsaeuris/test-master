036e0b3544e491145399910a43705ceef19b34d8
gattucci <gabriele.gattucci@euris.it>
2020-12-30 15:17:44 +0100

    check if find is present into file

--- build/scripts/replace-in-file.sh
+++ build/scripts/replace-in-file.sh
@@ -7,6 +7,14 @@ replace_with=$3
 
 echo "searching $find in $target_file"
 
+occurences=$(grep -o -i $find $target_file | wc -l)
+
+if [ $occurences = 0 ]
+then
+  echo "text $find was not found in $target_file, build aborted"
+  exit 1
+fi
+
 sed -i "/$find/c\ $replace_with" $target_file
 echo "$target_file contents:"
 cat $target_file
