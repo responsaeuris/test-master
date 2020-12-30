a897c8bbdd551227b5d1246bdffdc006a30d2cc7
gattucci <gabriele.gattucci@euris.it>
2020-12-30 14:49:55 +0100

    file array in package.json

--- build/scripts/create-npm-package.sh
+++ build/scripts/create-npm-package.sh
@@ -3,7 +3,13 @@ set -e # stops on first error
 
 echo "" && echo "***********************************************************" && /_/build/scripts/print-step.sh "INITIALIZING NPM PACKAGE"
 
-rm /_/package.json
+packageJson =/_/package.json
+rm $packageJson
+
 cd /_ && npm init -y
 
-cat /_/package.json
+find="\"license\": \"ISC\""
+replace="\"license\": \"ISC\", \"files\": [ \"app\" ]"
+/_/build/scripts/replace-in-file.sh $packageJson $find $replace
+
+cat $packageJson
