1d66555d7ab248d90ff444b83c38a7c89bad6f42
Finelli Davide <davide.finelli@euris.it>
2021-01-08 10:39:47 +0100

    remove payload additionalproperties

--- app/models/singleChoiceResource.js
+++ app/models/singleChoiceResource.js
@@ -69,7 +69,6 @@ module.exports.ResponsaSingleChoiceResource = {
     },
     payload: {
       type: 'object',
-      additionalProperties: false,
     },
     description: {
       type: 'string',
