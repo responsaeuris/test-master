03aa45582ddd995309c4370254bb221ccf3ac6cc
Finelli Davide <davide.finelli@euris.it>
2021-01-15 09:39:18 +0100

    expose rich message in swagger

--- app/index.js
+++ app/index.js
@@ -161,6 +161,7 @@ module.exports = fp(
           schemas: {
             ResponsaSingleChoiceResource,
             Error: errorSchema,
+            ResponsaRichMessageResource,
           },
         },
       },
