08e25dccd9ef14fc38d413fd43135e2860a79eb4
Finelli Davide <davide.finelli@euris.it>
2021-01-25 09:21:31 +0100

    build: fix typo

--- build/scripts/pre-build.sh
+++ build/scripts/pre-build.sh
@@ -11,7 +11,7 @@ then
   currentVersion=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
   echo "Current version: $currentVersion"
   bumpType="patch"
-  if [$GIT_BRANCH -eq "master"]
+  if [ $GIT_BRANCH -eq "master" ]
   then
     bumpType="minor"
   fi
