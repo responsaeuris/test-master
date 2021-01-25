39e8dee2ed9473f5dedc08c86ab1727a5a908208
Finelli Davide <davide.finelli@euris.it>
2021-01-25 10:25:44 +0100

    build: removed grep

--- build/scripts/pre-build.sh
+++ build/scripts/pre-build.sh
@@ -3,17 +3,10 @@ set -e # stops on first error
 
 echo "" && echo "***********************************************************" && /_/build/scripts/print-step.sh "CHECKING IF VERSION BUMPING IS NEEDED"
 cd /_/app
-ls -la
-echo "Current branch: $GIT_BRANCH"
-echo "Changed into app dir"
-git diff HEAD~1 HEAD -- package.json
-echo "done"
 gitOutput=$(git diff HEAD~1 HEAD -- package.json)
 echo "Git Output: $gitOutput"
-modVersion=$(echo $gitOutput | grep -c version)
-echo "ModVersion: $modVersion"
 
-if [ $modVersion -lt 2 ]
+if [ "${gitOutput}" == "" ]
 then
   echo "Version was not changed in this commit... Bumping new version"
   currentVersion=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
