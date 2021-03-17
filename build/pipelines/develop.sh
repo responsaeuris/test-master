5c5b0b08f2775fa8b5b6e5e9eeea8e92e0cd54e6
Davide Finelli <davide.finelli@euris.it>
2021-03-17 11:37:18 +0100

    build: add commit author and code analysis

--- build/pipelines/develop.sh
+++ build/pipelines/develop.sh
@@ -6,3 +6,10 @@ set -e # stops on first error
 /_/build/scripts/build-and-unit-test.sh
 
 /_/build/scripts/deploy.sh
+
+/_/build/scripts/sonarqube.sh plugin-core-js
+
+echo "Cleaning up artifacts"
+rm -rf test-report.xml
+rm -rf test-report-sonar.xml
+rm -rf coverage/lcov-report
