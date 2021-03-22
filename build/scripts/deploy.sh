54f1c9a809072c4a8bb2afab64ee857190654f01
Davide Finelli <davide.finelli@euris.it>
2021-03-22 15:39:13 +0100

    build: make CI fingerprint persistent

--- build/scripts/deploy.sh
+++ build/scripts/deploy.sh
@@ -7,6 +7,11 @@ cd /_
 echo "Add GitHub repo as local repo remote"
 git remote add --mirror=fetch secondary https://$PUBLIC_GIT_USERNAME:$PUBLIC_GIT_PASSWORD@$PUBLIC_GIT_REPO
 git fetch https://$GIT_USERNAME:$GIT_PASSWORD@$GIT_REPO
+
+# make CI fingerprint persistent
+/_/build/scripts/pre-build.sh
+git commit -am "$LAST_COMMIT CI fingerprint"
+
 echo "Pushing to GitHub"
 git push https://$PUBLIC_GIT_USERNAME:$PUBLIC_GIT_PASSWORD@$PUBLIC_GIT_REPO --all
 echo "done"
