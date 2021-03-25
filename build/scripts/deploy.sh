ce73a53d1ac354d1e24a8d9906ac5dd1099ad8a3
Davide Finelli <davide.finelli@euris.it>
2021-03-25 12:40:13 +0100

    build: substitute fetch with pull

--- build/scripts/deploy.sh
+++ build/scripts/deploy.sh
@@ -4,9 +4,10 @@ set -e # stops on first error
 echo "" && echo "***********************************************************" && /_/build/scripts/print-step.sh "PUBLISH TO GITHUB REPO"
 
 cd /_
+git pull https://$GIT_USERNAME:$GIT_PASSWORD@$GIT_REPO
+
 echo "Add GitHub repo as local repo remote"
 git remote add --mirror=fetch secondary https://$PUBLIC_GIT_USERNAME:$PUBLIC_GIT_PASSWORD@$PUBLIC_GIT_REPO
-git fetch https://$GIT_USERNAME:$GIT_PASSWORD@$GIT_REPO
 
 # make CI fingerprint persistent
 /_/build/scripts/pre-build.sh
