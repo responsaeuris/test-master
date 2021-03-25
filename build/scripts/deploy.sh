662db725d5533e1e52bd514fe6c098616197a6ba
Davide Finelli <davide.finelli@euris.it>
2021-03-25 12:54:43 +0100

    build: change public repo updating strategy

--- build/scripts/deploy.sh
+++ build/scripts/deploy.sh
@@ -4,18 +4,22 @@ set -e # stops on first error
 echo "" && echo "***********************************************************" && /_/build/scripts/print-step.sh "PUBLISH TO GITHUB REPO"
 
 cd /_
-git pull https://$GIT_USERNAME:$GIT_PASSWORD@$GIT_REPO
-
 echo "Add GitHub repo as local repo remote"
 git remote add --mirror=fetch secondary https://$PUBLIC_GIT_USERNAME:$PUBLIC_GIT_PASSWORD@$PUBLIC_GIT_REPO
+git fetch https://$GIT_USERNAME:$GIT_PASSWORD@$GIT_REPO
 
-# make CI fingerprint persistent
+echo "Pushing to GitHub"
+git push https://$PUBLIC_GIT_USERNAME:$PUBLIC_GIT_PASSWORD@$PUBLIC_GIT_REPO --all
+
+echo "make CI fingerprint persistent"
+cd /
+git clone --branch $GIT_BRANCH https://$PUBLIC_GIT_USERNAME:$PUBLIC_GIT_PASSWORD@$PUBLIC_GIT_REPO /public-repo
 /_/build/scripts/pre-build.sh
+cp /_/app/routes/status/statusRoute.js /public-repo/app/routes/status/statusRoute.js
+cd /public-repo
 git config --global user.email "$PUBLIC_GIT_USER_EMAIL"
 git config --global user.name "$PUBLIC_GIT_USERNAME"
 git add app/routes/status/statusRoute.js
 git commit -am "$LAST_COMMIT CI fingerprint"
-
-echo "Pushing to GitHub"
-git push https://$PUBLIC_GIT_USERNAME:$PUBLIC_GIT_PASSWORD@$PUBLIC_GIT_REPO --all
+git push origin $GIT_BRANCH
 echo "done"
