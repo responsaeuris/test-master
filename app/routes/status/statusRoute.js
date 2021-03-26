465114c92ae331507aaf2cbab9928d894f59eeeb
responsaeuris <responsaeuris@gmail.com>
2021-03-26 11:37:20 +0100

    pippo-master-1 CI fingerprint

--- app/routes/status/statusRoute.js
+++ app/routes/status/statusRoute.js
@@ -1,12 +1,1 @@
-const status = async () => {
-  const ciCommit = 'CI_PUTS_HERE_LAST_GIT_COMMIT'
-  const lastDeploy = 'CI_PUTS_HERE_DEPLOY_DATE'
-
-  return `ok! Plugin Core released on ${lastDeploy}, last commit was "${ciCommit}"`
-}
-
-module.exports = async function (fastify) {
-  fastify.get('/', status)
-}
-
-module.exports.status = status
+pippo
