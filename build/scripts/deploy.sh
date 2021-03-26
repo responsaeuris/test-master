b413d353dab612504568f5e06083e64e425a1c38
Davide Finelli <davide.finelli@euris.it>
2021-03-26 10:08:44 +0100

    build: abandon git mirror for copying on a single branch basis

--- build/scripts/deploy.sh
+++ build/scripts/deploy.sh
@@ -9,20 +9,26 @@ cd /public-repo
 
 echo "Add GitHub repo as local repo remote"
 git remote add -t $GIT_BRANCH secondary https://$PUBLIC_GIT_USERNAME:$PUBLIC_GIT_PASSWORD@$PUBLIC_GIT_REPO
-git push secondary $GIT_BRANCH
+
+echo "Setting public repo commit infos"
+git config --global user.email "$PUBLIC_GIT_USER_EMAIL"
+git config --global user.name "$PUBLIC_GIT_USERNAME"
+
+echo "Deleting public repo branch $GIT_BRANCH"
+set +e
+git push secondary --delete $GIT_BRANCH
+set -e
 
 echo "Updating status route"
 /_/build/scripts/pre-build.sh
 cp /_/app/routes/status/statusRoute.js /public-repo/app/routes/status/statusRoute.js
 
 echo "Committing to public repo"
-git config --global user.email "$PUBLIC_GIT_USER_EMAIL"
-git config --global user.name "$PUBLIC_GIT_USERNAME"
 git add app/routes/status/statusRoute.js
 git commit -m "$LAST_COMMIT CI fingerprint"
 
 echo "Pushing to GitHub"
-git push secondary $GIT_BRANCH
+git push secondary
 
 echo "done"
 
